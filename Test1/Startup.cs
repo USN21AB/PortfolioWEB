6cf50955eaa596a6cb84c715ab2575692bb4d34d
ShinobiSondre <48123393+ShinobiSondre@users.noreply.github.com>
2021-02-25 13:14:31 +0100

    ProfileBilde opplastning samtidig som oppdatere bruker

--- Test1/Startup.cs
+++ Test1/Startup.cs
@@ -14,12 +14,14 @@ namespace Test1
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
+        public Startup(IConfiguration configuration, Microsoft.AspNetCore.Hosting.IHostingEnvironment environment)
         {
             Configuration = configuration;
+            HostingEnvironment = environment;
         }
 
         public IConfiguration Configuration { get; }
+        public Microsoft.AspNetCore.Hosting.IHostingEnvironment HostingEnvironment { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
