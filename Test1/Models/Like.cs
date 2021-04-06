a938ecbe7249cf6ba1ce3041a6a3f958b6e0da07
Magnus Anfinnsen <magnus.selan@hotmail.com>
2021-04-06 16:06:55 +0200

    c

---
+++ Test1/Models/Like.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using Portefolio_webApp.Models;
+
+namespace Test1.Models
+{
+    public class Like
+    {
+        public string Id { get; set; }
+        public int Antall { get; set; }
+        public  List<string> Brukere { get; set; }
+
+        public Like()
+        {
+            Brukere = new List<string>();
+        }
+    }
+}
