e104901886e9bd78d461bc1e4906f37859ccd176
dizzymcfire <fredrik.ska@gmail.com>
2021-04-28 12:41:10 +0200

    riktig rekkefølge på forside og profilside

---
+++ Test1/Models/RecentUploadList.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using Test1.Models;
+
+namespace Portefolio_webApp.Models
+{
+    public class RecentUploadList
+    {
+        public string Id { get; set; }
+
+        public string Dato { get; set; }
+
+        public string Klokkeslett { get; set; }
+    }
+}
